1ca8972616c16b2ef882af57deb8745b8fb75309
Dmytro-Krayniy <105720776+Dmytro-Krayniy@users.noreply.github.com>
2023-02-26 15:39:19 +0200

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,12 +1,9 @@
 import time
-
 from selenium.webdriver.common.by import By
 
 
-def test_basket_button(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    basket_button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
-    time.sleep(30)
-    assert basket_button, 'Button "Add to basket" not found'
-
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
